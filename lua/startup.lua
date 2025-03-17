74cc8e72d2c211dccaee346d46802630cb97f307
Thomas Kroll <zhivotnoya@gmail.com>
2025-03-17 14:17:18 -0400

    Update startup.lua
    removed whitespace.

--- lua/startup.lua
+++ lua/startup.lua
@@ -3,21 +3,16 @@ print( "///////////////////////////////\n" )
 config = TebexConfig:init()
 
 RegisterCommand("tebex", function(ply, args, raw)
-
 	if (args[1] == nil) then
 		--Help!
 		return
 	end
-
 	if (Tebex.commands[args[1]] == nil) then
 		print( "Unknown command \"tebex " .. args[1] .. "\"" )
 		return
 	end
-
 	Tebex.commands[args[1]](ply, args);
-
 end, true)
-
 
 Tebex.doCheck = function ()
 	if ((os.time() - Tebex.lastCalled) > Tebex.nextCheck) then
@@ -25,8 +20,6 @@ Tebex.doCheck = function ()
 		Tebex.commands["forcecheck"](nil, {"forcecheck"})
 	end
 end
-
-
 
 if (config:get("secret") == "") then
 	Tebex.err( "You have not yet defined your secret key. Use tebex secret <secret> to define your key" )
@@ -36,10 +29,7 @@ if (config:get("secret") == "") then
 	end)
 end
 
-
-
 Tebex.ok("Starting Tebex_FiveM 0.1")
-
 Citizen.CreateThread(function()
 	while true do
 		if (config:get("secret") ~= "") then
@@ -48,5 +38,3 @@ Citizen.CreateThread(function()
 		Citizen.Wait(10000)
 	end
 end)
-
-
